6730f5a4ce72bbd81f4cef1b1fb7057b2a76d61b
Ryan Raber <rcraber@gmail.com>
2022-03-27 02:35:46 -0400

    Moved Constants into Seperate File

--- Dynamics.py
+++ Dynamics.py
@@ -2,75 +2,7 @@ import numpy as np
 from RotationalMatricies import *
 from Wind import *
 from scipy.integrate import solve_ivp
-
-
-#Forces and Moments 
-Jx = 0.824
-Jy = 1.135
-Jz = 1.759
-Jxz= 0.120 #Moment Coupling Constant
-
-#Calculate Gamma
-Gamma=Jx*Jz-Jxz**2
-Gamma_1=Jxz*(Jx-Jy+Jz)/Gamma
-Gamma_2=(Jz*(Jz-Jy)+Jxz**2)/Gamma
-Gamma_3=Jz/Gamma
-Gamma_4=Jxz/Gamma
-Gamma_5=(Jz-Jx)/Jy
-Gamma_6=Jxz/Jy
-Gamma_7=((Jx-Jy)*Jx+Jxz**2)/Gamma
-Gamma_8=Jx/Gamma
-
-#Aircraft Constants
-S_wing        = 0.55
-b             = 2.90
-c             = 0.19
-S_prop        = 0.2027
-rho           = 1.2682
-e             = 0.9
-AR            = b**2/S_wing
-C_L_0         = 0.23
-C_D_0         = 0.043
-C_m_0         = 0.0135
-C_L_alpha     = 5.61
-C_D_alpha     = 0.030
-C_m_alpha     = -2.74
-C_L_q         = 7.95
-C_D_q         = 0.0
-C_m_q         = -38.21
-C_L_delta_e   = 0.13
-C_D_delta_e   = 0.0135
-C_m_delta_e   = -0.99
-M             = 50
-alpha0        = 0.47
-epsilon       = 0.16
-C_D_p         = 0.0
-C_Y_0         = 0.0
-C_ell_0       = 0.0
-C_n_0         = 0.0
-C_Y_beta      = -0.98
-C_ell_beta    = -0.13
-C_n_beta      = 0.073
-C_Y_p         = 0.0
-C_ell_p       = -0.51
-C_n_p         = -0.069
-C_Y_r         = 0.0
-C_ell_r       = 0.25
-C_n_r         = -0.095
-C_Y_delta_a   = 0.075
-C_ell_delta_a = 0.17
-C_n_delta_a   = -0.011
-C_Y_delta_r   = 0.19
-C_ell_delta_r = 0.0024
-C_n_delta_r   = -0.069
-C_prop        = 1
-k_motor       = 80 #80
-k_T_p         = 0
-k_omega       = 0
-
-gravity       = 9.806650
-mass          = 13.5
-
+from Constants import *
 
 def Forces_and_Moments(states, control_input, dt):
     #Unpack States
@@ -129,7 +61,7 @@ def Forces_and_Moments(states, control_input, dt):
 
 
     #Calculate Aerodynamic Torques
-    tau_phi   = 1/2*rho*Va**2*S_wing*b*(C_ell_0 + C_ell_beta*beta + C_ell_p*p*b/(2*Va) + C_ell_r*r*b/(2*Va) + C_ell_delta_a*delta_a + C_ell_delta_r*delta_r)
+    tau_phi   = 1/2*rho*Va**2*S_wing*b*(C_l_0 + C_l_beta*beta + C_l_p*p*b/(2*Va) + C_l_r*r*b/(2*Va) + C_l_delta_a*delta_a + C_l_delta_r*delta_r)
     tau_theta = 1/2*rho*Va**2*S_wing*c*(C_m_0 + C_m_alpha*alpha + C_m_q*c*q/(2*Va) + C_m_delta_e*delta_e)
     tau_psi   = 1/2*rho*Va**2*S_wing*b*((C_n_0+C_n_beta*beta) + (C_n_p*p + C_n_r*r)*b/(2*Va) + C_n_delta_a*delta_a + C_n_delta_r*delta_r)
 
